d62e362f33340baff6b567de36795b7812b10f47
Stanley Wang <wang4928@purdue.edu>
2023-07-16 20:58:04 -0500

    Added index.ts for exporting components and changed layout to have Navbar and Footer

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+}
