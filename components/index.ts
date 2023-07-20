c5eb2b85ad6300ede38cd2fc5d6edaa1418ac387
Stanley Wang <wang4928@purdue.edu>
2023-07-19 21:42:15 -0500

    Rapid API cars and added car cards components

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
@@ -14,4 +16,6 @@ export {
   SearchBar,
   CustomFilter,
   SearchManufacturer,
+  CarCard,
+  CarDetails,
 }
